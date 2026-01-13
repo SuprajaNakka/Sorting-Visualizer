073bf5a38b1f033b313cd158b280b99663276929
SuprajaNakka <suprajanakka2985@gmail.com>
2026-01-13 20:59:46 +0530

    Add files via upload

--- Selection Sort/index.js
+++ Selection Sort/index.js
@@ -1,17 +1,69 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Sorting Visualizer</title>
-    <link href="style.css" rel="stylesheet" />
-</head>
-<body>
-    <section class="head">Selection Sort Visualizer</section>
-    <section class="data-container"></section>
-    <button class="btn1" onclick="generate()" id="Button1">Generate New Array</button>
-    <button class="btn2" onclick="SelectionSort(),disable()" id="Button2">Selection Sort</button>
-</body>
-<script src="index.js"></script>
-</html>
+const container = document.querySelector(".data-container");
+
+function generatebars(num = 20) {
+    for (let i = 0; i < num; i += 1) {
+        const value = Math.floor(Math.random() * 100) + 1;
+        const bar = document.createElement("div");
+        bar.classList.add("bar");
+        bar.style.height = `${value * 3}px`;
+        bar.style.transform = `translateX(${i * 30}px)`;
+
+        const barLabel = document.createElement("label");
+        barLabel.classList.add("bar__id");
+        barLabel.innerHTML = value;
+
+        bar.appendChild(barLabel);
+        container.appendChild(bar);
+    }
+}
+
+async function InsertionSort(delay = 800) {
+    let bars = document.querySelectorAll(".bar");
+    bars[0].style.backgroundColor = "rgb(49, 226, 13)";
+    for (var i = 1; i < bars.length; i += 1) {
+        var j = i - 1;
+        var key = parseInt(bars[i].childNodes[0].innerHTML);
+        var height = bars[i].style.height;
+        var barval = document.getElementById("ele");
+        barval.innerHTML = `<h3>Element Selected is :${key}</h3>`;
+        bars[i].style.backgroundColor = "darkblue";
+
+        await new Promise((resolve) => setTimeout(() => resolve(), delay));
+
+        while (j >= 0 && parseInt(bars[j].childNodes[0].innerHTML) > key) {
+            bars[j].style.backgroundColor = "darkblue";
+            bars[j + 1].style.height = bars[j].style.height;
+            bars[j + 1].childNodes[0].innerText = bars[j].childNodes[0].innerText;
+            j = j - 1;
+            await new Promise((resolve) => setTimeout(() => resolve(), delay));
+            for (var k = i; k >= 0; k--) {
+                bars[k].style.backgroundColor = "rgb(49, 226, 13)";
+            }
+        }
+
+        bars[j + 1].style.height = height;
+        bars[j + 1].childNodes[0].innerHTML = key;
+        await new Promise((resolve) => setTimeout(() => resolve(), delay));
+
+        bars[i].style.backgroundColor = "rgb(49, 226, 13)";
+    }
+
+    barval.innerHTML = "<h3>Sorted!!!</h3>";
+    document.getElementById("Button1").disabled = false;
+    document.getElementById("Button1").style.backgroundColor = "#6f459e";
+    document.getElementById("Button2").disabled = false;
+    document.getElementById("Button2").style.backgroundColor = "#6f459e";
+}
+
+generatebars();
+
+function generate() {
+    window.location.reload();
+}
+
+function disable() {
+    document.getElementById("Button1").disabled = true;
+    document.getElementById("Button1").style.backgroundColor = "#d8b6ff";
+    document.getElementById("Button2").disabled = true;
+    document.getElementById("Button2").style.backgroundColor = "#d8b6ff";
+}
